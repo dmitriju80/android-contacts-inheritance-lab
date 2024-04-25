baaad9815ec2f5d9b6e8ba75d3052174dea023ae
UshakovDV <dmitriju80@gmail.com>
2024-04-25 21:16:20 +0300

    продолжение ветки DEV address

---
+++ app/Address.java
@@ -0,0 +1,11 @@
+public class Address {
+    private int address;
+    private String type;
+    private int limit;
+
+    public Address (int address,String type,int limit){
+        this.address=address;
+        this.type=type;
+        this.limit=limit;
+    }
+}
