56557fd412764f3f822214043aa6360e9c8bb077
UshakovDV <dmitriju80@gmail.com>
2024-04-25 21:06:32 +0300

    ветка person

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/person.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+public class person {
+    private String name;
+    private int age;
+
+    public person (String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+}
